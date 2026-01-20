bd66032ec8a1d310f088bf9ced4fb8456dfc170f
Mehmet Kutay Bozkurt <me@mkutay.dev>
2026-01-20 22:30:40 +0000

    feat: add functionality to count completed actions for today

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { isNotNull, isNull } from "drizzle-orm";
+import { endOfDay, startOfDay } from "date-fns";
+import { and, gte, isNotNull, isNull, lt } from "drizzle-orm";
 import { db } from "@/lib/db/drizzle";
 import { actions, projects } from "@/lib/db/schema";
 
@@ -8,10 +9,23 @@ export const revalidate = 300;
 export default async function Home() {
   const acs = await db.select().from(actions).where(isNull(actions.completed));
 
+  const now = new Date();
+
   const completed = await db
     .select()
     .from(actions)
     .where(isNotNull(actions.completed));
+
+  const completedToday = await db
+    .select()
+    .from(actions)
+    .where(
+      and(
+        isNotNull(actions.completed),
+        gte(actions.completed, startOfDay(now)),
+        lt(actions.completed, endOfDay(now)),
+      ),
+    );
 
   const prjs = await db
     .select()
@@ -25,7 +39,7 @@ export default async function Home() {
       </p>
       <p className="text-lg leading-relaxed">active projects: {prjs.length}</p>
       <p className="text-lg leading-relaxed">
-        completed actions: {completed.length}
+        completed actions: {completed.length} (today: {completedToday.length})
       </p>
     </div>
   );
